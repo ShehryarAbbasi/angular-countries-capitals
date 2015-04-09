2ff883ca8f7713d8c27058b0e37cbb45f3ca0488
ShehryarAbbasi <shehryar.abbasi@gmail.com>
2015-04-08 23:06:40 -0500

    add list of countries

--- app/cac-app.js
+++ app/cac-app.js
@@ -6,30 +6,64 @@ angular.module('CaCApp', ['ngRoute'])
 			controller : 'HomeCtrl'
 		}).when('/countries', {
 			templateUrl : 'countries.html',
-			controller : 'CountriesCtrl'
-		}).otherwise({
-			redirectTo : '/'
+			controller : 'AllCountriesCtrl'
+		}).when('/countries/:country/:capital', {
+			templateUrl : 'country.html',
+			controller : 'OneCountryCtrl',
+			resolve : {
+				country: function($route, $location) {
+					var country= $route.current.params.country;
+					return country;
+				},
+
+				capital: function($route, $location) {
+					var capital = $route.current.params.capital;
+					return capital;
+				}
+			}
 		});
 
 	}) // end of config
-	.service('geoService', function($http){
+	.factory('GeoAllCountries', function($http){
 		
-		var userName = "sabbasi"
-		var url = "http://api.geonames.org/countryInfo?type=JSON&" + "username=" + userName;
+		var userName = "sabbasi";
+		var urlAllCountries = "http://api.geonames.org/countryInfo?type=JSON&" + "username=" + userName;
+
+		return $http.get(urlAllCountries);
+
+	}) //end of Factory
+
+	.factory('GeoOneCountry', function($http){
 		
-		return $http.get(url);
+		var userName = "sabbasi";
+		var urlOneCountry = "http://api.geonames.org/search?type=JSON" + "&username=" + userName;
 
+		var GeoOneCountry = {
+			findCountry: function(country, capital) {
+				return $http.get(urlOneCountry + "&q=" + country + "&name=" + capital + "&isNameRequired=true");
+			}
+		};
 
-	}) //end of Service
+		return GeoOneCountry;
+		//return $http.get(urlOneCountry);
+
+	}) //end of Factory
 
 	.controller('HomeCtrl', function($scope){
 		console.log("home controller");
 
 	})
-	.controller('CountriesCtrl', function($scope, geoService){
-		geoService.then(function(response){
-			$scope.countries = response.data.geonames;
+	.controller('AllCountriesCtrl', function($scope, GeoAllCountries){
+		GeoAllCountries.success(function(response){
+			$scope.countries = response.geonames;
 		});
 		
-
+	})
+	.controller('OneCountryCtrl', function($scope, country, capital, GeoOneCountry){
+		$scope.country = country;
+		$scope.capital = capital;
+		GeoOneCountry.findCountry(country, capital).success(function(response){
+			$scope.oneCountry = response.geonames;
+			console.log($scope.oneCountry);
+		});
 	});
